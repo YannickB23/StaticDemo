21ef9146c7f7c7df98a982d04eb3e8ff1f7e3f67
YANNICK BETERAMS <YANNICK.BETERAMS@student.intecbrussel.be>
2020-11-17 12:10:58 +0100

    Add project files.

---
+++ ClassLibraryDemo/Helper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticDemo
+{
+    public static class Helper
+    {
+        public static void ShowStudents(Student student)
+        {
+            Console.WriteLine("----------------------");
+            Console.WriteLine(student.FirstName);
+            Console.WriteLine(student.LastName);
+        }
+    }
+}
---
+++ StaticDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First name pls?");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Second name pls.");
+            string lastName = Console.ReadLine();
+
+            Student student = new Student();
+            student.FirstName = firstName;
+            student.LastName = lastName;
+
+            Helper.ShowStudents(student);
+        }
+    }
+}
